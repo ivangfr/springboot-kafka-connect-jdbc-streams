0e4aa7622b9fb61c42fd571cb6d1c441ea755ae9
Ivan Franchin <ivan.franchin@takeaway.com>
2018-12-26 18:44:02 -0200

    initial commit

---
+++ store-kafka-streams/src/main/java/com/mycompany/storekafkastreams/StoreKafkaStreamsApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.storekafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreKafkaStreamsApplication.class, args);
+	}
+
+}
+
---
+++ store-kafka-streams/src/test/java/com/mycompany/storekafkastreams/StoreKafkaStreamsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mycompany.storekafkastreams;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreKafkaStreamsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
